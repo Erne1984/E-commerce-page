1957990e2d53307df9b513ae3133fc595e52d355
Erne1984 <ernestofamorim@gmail.com>
2024-07-02 09:51:11 -0300

    working on header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,29 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Header.css'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header({ }) {
     return (
         <>
-            <nav>
-                <div className='right-column'>
-                    <span className="logo">SNEAKERS</span>
+            <nav className='nav-header'>
+                <div className='left-column'>
+                    <h1 className="logo">SNEAKERS</h1>
 
-                    <div>
-                        <ul>
-                            <li>Collections</li>
-                            <li>Men</li>
-                            <li>Woman</li>
-                            <li>About</li>
-                            <li>Contact</li>
-                        </ul>
-                    </div>
+                    <ul className='list-itens'>
+                        <li>Collections</li>
+                        <li>Men</li>
+                        <li>Woman</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+
+                <div className='right-column'>
+
+                    <FontAwesomeIcon className='cart' icon={faCartShopping} />
+
+                    <img src='./image-avatar.png' className='avatar'></img>
+
                 </div>
 
             </nav>
