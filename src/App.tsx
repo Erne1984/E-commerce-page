f50d1ac6c1f1c5ce0e044dbd0fcd9bd5762b8ff9
Erne1984 <ernestofamorim@gmail.com>
2024-07-03 10:05:20 -0300

    working on gallery

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,43 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-import Header from './components/Header/Header'
+import Header from './components/Header/Header';
+import Gallery from './components/Galery/Gallery';
+import { ProductsImg } from './types/ProductsImg';
+
+const productsImgsUrl: ProductsImg[] = [
+  {
+    mainImgUrl: "./products/image-product-1.jpg",
+    thumbnailUrl: "./products/image-product-1-thumbnail.jpg"
+  },
+  {
+    mainImgUrl: "./products/image-product-2.jpg",
+    thumbnailUrl: "./products/image-product-2-thumbnail.jpg"
+  },
+  {
+    mainImgUrl: "./products/image-product-3.jpg",
+    thumbnailUrl: "./products/image-product-3-thumbnail.jpg"
+  },
+  {
+    mainImgUrl: "./products/image-product-4.jpg",
+    thumbnailUrl: "./products/image-product-4-thumbnail.jpg"
+  },
+];
 
 function App() {
+  const [currentMainImg, setCurrentImg] = useState(productsImgsUrl[0].mainImgUrl)
 
   return (
     <>
-      <Header/>
+      <Header />
+      <main>
+        <Gallery 
+        mainImgUrl={currentMainImg} 
+        imgsRow={productsImgsUrl} 
+        setCurrentImg={setCurrentImg}/>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/types/ProductsImg.tsx
@@ -0,0 +1,4 @@
+export interface ProductsImg {
+    mainImgUrl: string;
+    thumbnailUrl: string;
+}  
